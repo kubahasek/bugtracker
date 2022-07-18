1ca7ffee68da1591119c390030de60c6aa2a3bb5
Jakub Hašek <hasek.kuba@hotmail.cz>
2022-07-18 19:40:21 +0200

    removed link from project card

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -1,5 +1,4 @@
 import { Project } from "@prisma/client";
-import Link from "next/link";
 import React from "react";
 
 type Props = {
@@ -12,16 +11,14 @@ function addLeadingZeros(num: number, totalLength: number) {
 
 const ProjectCard = ({ project }: Props) => {
   return (
-    <Link href={`/projects/${project.id}`}>
-      <div className="w-full border-2 border-gray-500 p-5 rounded-lg mt-4 hover:border-gray-50 hover:cursor-pointer transition-all">
-        <div className="flex flex-row items-center justify-between">
-          <h1>#{addLeadingZeros(project.id, 3)}</h1>
-          <div className="text-left w-4/5 ml-4">
-            <h1 className=" dark:text-white">{project.name}</h1>
-          </div>
+    <div className="w-full border-2 border-gray-500 p-5 rounded-lg mt-4 hover:border-gray-50 hover:cursor-pointer transition-all">
+      <div className="flex flex-row items-center justify-between">
+        <h1>#{addLeadingZeros(project.id, 3)}</h1>
+        <div className="text-left w-4/5 ml-4">
+          <h1 className=" dark:text-white">{project.name}</h1>
         </div>
       </div>
-    </Link>
+    </div>
   );
 };
 
